306d1f00f7a325fc91dd2bf148240a1c9303fae3
AKWANA686 <akwanaoliver@gmail.com>
2022-06-21 09:18:12 +0000

    a function that prints the first x elements of a list and only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    try:
-        div = a / b
-    except (ZeroDivisionError, FloatingPointError):
-        div = None
-    finally:
-        print("Inside result: {}".format(div))
-    return div
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        print()
+        return count
