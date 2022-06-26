a8b396fe0e21940c2d1008be5d9677c43c8ca175
AKWANA686 <akwanaoliver@gmail.com>
2022-06-26 08:14:42 +0000

    a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Rectangle with size"""
+
+
+class Rectangle:
+    """Representation of a rectangle"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
