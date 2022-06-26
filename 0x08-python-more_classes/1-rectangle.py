1edc1a65eba5f3bd1fbadb13c7942393d278a0e7
AKWANA686 <akwanaoliver@gmail.com>
2022-06-26 08:53:01 +0000

    a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,6 +5,40 @@
 class Rectangle:
     """Representation of a rectangle"""
 
-    def __init__(self, size):
+    def __init__(self, width=0, height=0):
         """Instantiation with size"""
-        self.__size = size
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Property to retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property to retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property setter to set width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Property to retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Property setter to set height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
