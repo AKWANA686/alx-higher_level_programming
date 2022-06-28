fb52306b338d624abd417f3573aae0bbfc228578
AKWANA686 <akwanaoliver@gmail.com>
2022-06-28 09:54:49 +0000

    a class Square that defines a square by: (based on 2-square.py)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,6 +9,8 @@ class Square:
         """Instantiation with optional size"""
         if type(size) != int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
