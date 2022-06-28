d84052110f6eab641335d334b34a2c71721a28ce
AKWANA686 <akwanaoliver@gmail.com>
2022-06-28 12:57:11 +0000

    LockedClass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
-LockedClass
+This is a module that containts a clas that avoids
+dynmaically created attributes
 """
 
 
 class LockedClass:
-    """ No class or object attributes, can't set
-        Except for first_name
-    """
-    def __setattr__(self, attribute, value):
-        if attribute == "first_name":
-            self.__dict__[attribute] = value
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Init method """
+        pass
